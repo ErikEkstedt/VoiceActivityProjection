78d64ef845d0c891eadbaeb4cd90352bfa2cdc32
Erik <eeckee@gmail.com>
2023-01-06 12:50:41 +0100

    added sds

---
+++ sds/zmk_receiver.py
@@ -0,0 +1,34 @@
+import zmq
+import time
+
+"""
+Server is paired to `audio_zmq.py`
+"""
+
+context = zmq.Context()
+
+
+def simple_server(port: int = 5558, topic: str = "tt_probs"):
+    socket = context.socket(zmq.SUB)
+    socket.connect(f"tcp://localhost:{port}")
+    socket.setsockopt_string(zmq.SUBSCRIBE, topic)
+
+    i = 0
+    while True:
+        topic = socket.recv_string()
+        # d = socket.recv_pyobj()
+        d = socket.recv()
+        print("received: ", type(d), d)
+        time.sleep(0.1)
+        i += 1
+
+
+if __name__ == "__main__":
+
+    try:
+        simple_server()
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt")
+    finally:
+        context.destroy()
+        print("Context destroyed")
